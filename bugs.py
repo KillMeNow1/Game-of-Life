16faa610b0cf7815c36641ebec2dc35193ed3eca
Live session user <anna.olerinyova@sjc.ox.ac.uk>
2016-11-11 06:06:53 +0000

    NEarly done different codes

--- bugs.py
+++ bugs.py
@@ -5,18 +5,25 @@
 import numpy as np
 import sys 
 
-n = 4 #the size of the 2D array for both animals and food arrays
-time = 1 #length of test run
-field = np.ones((n,n)) #initialisig 2D array of size n representing amount of food, set to 0
-#initialising 2D array representing presence of an animal with random distribution of 0 and 1; 0 means dead, 1 means alive
-animals = np.random.randint(2, size = (n,n))
-#animals = np.ones ((n,n))
-#print (field[0,0])
+#n = raw_input("What's the size of our game?") 
+n = 10 #the size of the 2D array for both animals and food arrays
+time = 3 #length of test run
+field = np.zeros((n,n)) #initialisig 2D array of size n representing amount of food, set to 
+#initialising 2D array representing presence of an animal with random distribution of 0 and 1; 0 means dead, 1 means alive, in subset of field
+s = 2 #space around population
+#have this as input and evaluate by if statement
+population = np.random.randint(2, size = (n-2*s,n-2*s))
+empty_1 = np.zeros((n-2*s,s))
+empty_2 = np.zeros((s,n))
+anim_1 = np.hstack((empty_1, population, empty_1)) 	#adding buffer with 0s of width s to the left and right of population
+animals = np.vstack((empty_2, anim_1, empty_2))		#buffer of 2 rows of 0s on top and bottom; now animals dimensions match field
+
 print "The initial animals grid is:\n", animals 
 a = 2 # arbitrary constant for rate of food growth
+b = 3 # rate of replication
 c = 1 # arbitrary constant for rate of food consumption by an animal
 
-
+field += n-2*s
 for t in range (0,time): #our set run time 
 	field += a
 	#print "Time is:", t, "and field is:", field
@@ -25,45 +32,58 @@ for t in range (0,time): #our set run time
 		#print animals
 		for y in range (0,n): 
 			#if animal alive, all positions distant by 1 around field(x,y) decrease by c if animal present in animals (x,y)
-			if (animals[x,y] == 1): 
-				#print field, animals	
+			if animals[x,y] == 1: 	
 				#special case for corners and edge rows/columns as can't have negative values or values larger than n		
-				if (x == 0):	
-					for x_poz in range (0,2):	
-						if (y == 0):	
-							for y_poz in range (0,2): 
-								field[x_poz, y_poz] -= c
+				if x == 0:	
+					for x_poz in range (0,x+2):	
+						if y == 0:	
+							for y_poz in range (0,y+2): 
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c
 									
 						elif y > 0 and y < n-1:
 							for y_poz in range ( y-1, y+2):		
-								field[x_poz, y_poz] -= c	#all rows except for first and last
-						elif (y == n-1):
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c	#all rows except for first and last
+						elif y == n-1:
 							for y_poz in range (n-2, n):
-								field[x_poz, y_poz] -= c	#once y == n, i.e. last row
-				elif (y == 0): 		
-					for y_poz in range (0,2):
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c	#once y == n, i.e. last row
+				elif y == 0: 		
+					for y_poz in range (0,y+2):
 						if 0 < x and x < n-1:
 							for x_poz in range (x-1, x+2):		
-								field[x_poz, y_poz] -= c 	# don't need if x == 0 as (0,0) covered previously	
-						elif (x == n-1):
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c 	# don't need if x == 0 as (0,0) covered previously	
+						elif x == n-1:
 							for x_poz in range (n-2, n): 
-								field[x_poz, y_poz] -= c
-				elif (x == n-1): 		
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c
+				elif x == n-1: 		
 					for x_poz in range (n-2,n):
 						if 0 < y and y < n-1:
 							for y_poz in range ( y-1, y+2):		
-								field[x_poz, y_poz] -= c
-						elif (y == n-1):
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c
+						elif y == n-1:
 							for y_poz in range (n-2, n):  
-								field[x_poz, y_poz] -= c
-				elif (y == n-1):		
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c
+				elif y == n-1:		
 					for y_poz in range (n-2,n):
 						if 0 < x and x < n-1:
-							for x_poz in range ( x-1, x+1):		
+							for x_poz in range ( x-1, x+2):		
+								if field[x_poz, y_poz] >= c:
+									field[x_poz, y_poz] -= c
+				else:
+					for x_poz in range (x-1, x+2):
+						for y_poz in range (y-1, y+2):
+							if field[x_poz, y_poz] >=c:
 								field[x_poz, y_poz] -= c
-				#else:
-					#for x_poz in range (x-1, x+2):
-						#for y_poz in range (y-1, y+2):
-							#field[x_poz, y_poz] -= c
-							
-print "At time 1, the field is:\n",field
+			
+	
+			#else:
+				#if t%b == 0:
+				
+								
+print "At time ",t+1, "the field is:\n",field
--- new.py
+++ new.py
@@ -5,67 +5,65 @@
 import numpy as np
 import sys 
 
+#n = raw_input("What's the size of our game?") 
 n = 5 #the size of the 2D array for both animals and food arrays
-field = np.zeros ((n,n)) #initialisig 2D array of size n representing amount of food, set to 0
+time = 2 #length of test run
+field = np.ones((n,n)) #initialisig 2D array of size n representing amount of food, set to 4
 #initialising 2D array representing presence of an animal with random distribution of 0 and 1; 0 means dead, 1 means alive
-animals = np.random.randint(2, size = (n,n)) 
-#print (field)
-#print (animals)
+animals = np.random.randint(2, size = (n,n))
+
+print "The initial animals grid is:\n", animals 
 a = 2 # arbitrary constant for rate of food growth
 c = 1 # arbitrary constant for rate of food consumption by an animal
 
 
-for t in range (0,100): #our initial run time of 100
-	field += a	#amount a of food is added with every iteration for all positions in field 
-	for x in range (0,5):	
-		for y in range (0,5): 
+for t in range (0,time): #our set run time 
+	field += a
+	#print "Time is:", t, "and field is:", field
+	#amount a of food is added with every iteration for all positions in field 
+	for x in range (0,n):	
+		#print animals
+		for y in range (0,n): 
 			#if animal alive, all positions distant by 1 around field(x,y) decrease by c if animal present in animals (x,y)
-			if animals (x,y) == 1: 	
+			if (animals[x,y] == 1): 
+				#print field, animals	
 				#special case for corners and edge rows/columns as can't have negative values or values larger than n		
-				if (x == 0):		
-					for x_poz in range (0,2):
-						if (y == 0):					
+				if (x == 0):	
+					for x_poz in range (0,2):	
+						if (y == 0):	
 							for y_poz in range (0,2): 
-								field(x_poz, y_poz) -= c	
-						elif (0 < y < n):
+								field[x_poz, y_poz] -= c
+									
+						elif y > 0 and y < n-1:
 							for y_poz in range ( y-1, y+2):		
-								field(x_poz, y_poz) -= c	#all rows except for first and last
-						elif (y == n):
-							for y_poz in range (n-1, n+1):  	
-								field(x_poz, y_poz) -= c	#once y == n, i.e. last row
+								field[x_poz, y_poz] -= c	#all rows except for first and last
+						elif (y == n-1):
+							for y_poz in range (n-2, n):
+								field[x_poz, y_poz] -= c	#once y == n, i.e. last row
 				elif (y == 0): 		
 					for y_poz in range (0,2):
-						# don't need if x == 0 as (0,0) covered previously	
-						if (0 < x < n):
+						if 0 < x and x < n-1:
 							for x_poz in range (x-1, x+2):		
-								field(x_poz, y_poz) -= c
-						elif (x == n):
-							for x_poz in range (n-1, n+1): 
-								field(x_poz, y_poz) -= c
-				elif (x == n): 		
-					for x_poz in range (n-1,n+1):
-						if (0 < y < n):
+								field[x_poz, y_poz] -= c 	# don't need if x == 0 as (0,0) covered previously	
+						elif (x == n-1):
+							for x_poz in range (n-2, n): 
+								field[x_poz, y_poz] -= c
+				elif (x == n-1): 		
+					for x_poz in range (n-2,n):
+						if 0 < y and y < n-1:
 							for y_poz in range ( y-1, y+2):		
-								field(x_poz, y_poz) -= c
-						elif (y == n):
-							for y_poz in range (n-1, n+1):  
-								field(x_poz, y_poz) -= c
-				elif (y == n):		
-					for y_poz in range (n-1,n+1):
-						if (0 < x < n):
-							for x_poz in range ( x-1, x+2):		
-								field(x_poz, y_poz) -= c
-				else:
-					for x_poz in range (x-1, x+2):
-						for y_poz in range (y-1, y+2):
-							field(x_poz, y_poz) -= c
-	if (t%10 == 0):
-		print (field), (animals)
-					
-					
-					
-						
-						
-						
-						
+								field[x_poz, y_poz] -= c
+						elif (y == n-1):
+							for y_poz in range (n-2, n):  
+								field[x_poz, y_poz] -= c
+				elif (y == n-1):		
+					for y_poz in range (n-2,n):
+						if 0 < x and x < n-1:
+							for x_poz in range ( x-1, x+1):		
+								field[x_poz, y_poz] -= c
+				#else:
+					#for x_poz in range (x-1, x+2):
+						#for y_poz in range (y-1, y+2):
+							#field[x_poz, y_poz] -= c
 							
+print "At time 1, the field is:\n",field
